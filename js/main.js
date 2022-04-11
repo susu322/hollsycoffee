33c88df67b1c28e6d8f1798865854ca029bd5072
susu322 <103081312+susu322@users.noreply.github.com>
2022-04-11 11:52:54 +0900

    hollys

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(function(){
+    $('.slide_img').slick({ //메인슬라이드
+      arrows : true, 
+      dots: true,
+      slidesToShow : 1,
+      speed : 500
+    });
+
+    $(".newsbox_list").slick({ //소식 바
+      vertical : true,
+      rows:1,
+      slidesToShow : 1,
+      arrows:false,
+      autoplay : true,
+      autoplaySpeed : 1000
+    });
+
+    $('.menu_list').slick({ // 메뉴 슬라이드
+      infinite: true,
+      speed: 300,
+      slidesToShow: 4,
+      centerMode: true,
+      arrows : true
+    });
+
+    $('.sns_list').slick({ // 인스타그램
+      infinite: true,
+      slidesToShow: 3,
+      arrows : false,
+      autoplay: true,
+      autoplaySpeed : 2800,
+      pauseOnHover : true
+    });
+
+
+  })//ready
